dc98c19af78e598a56ab5272fcb3126b732269b8
isabuitrago1223 <buitragoisa1223@gmail.com>
2026-08-04 16:03:34 -0500

    Agregue el ejercicio 4 al main para poder ejecutarlo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from ejercicios.ejercicio01 import main as ejercicio1
 from ejercicios.ejercicio02 import main as ejercicio2
 from ejercicios.ejercicio03 import main as ejercicio3
+from ejercicios.ejercicio04 import main as ejercicio4
 
 
 # Función que muestra el menú principal
@@ -13,6 +14,7 @@ def mostrar_menu():
     print("1. Ejercicio 1 - Registro de temperaturas")
     print("2. Ejercicio 2 - Análisis de ventas mensuales")
     print("3. Ejercicio 3 - Análisis de calificaciones")
+    print("4. Ejercicio 4 - Inventario inteligente")
     print("0. Salir")
 
 
@@ -22,34 +24,32 @@ def main():
     # Ciclo que mantiene el programa en ejecución
     while True:
 
-        # Mostrar el menú de opciones
+        # Mostrar el menú
         mostrar_menu()
 
-        # Solicitar al usuario una opción
+        # Solicitar una opción
         opcion = input("\nSeleccione una opción: ")
 
-        # Ejecutar el ejercicio 1
         if opcion == "1":
             ejercicio1()
 
-        # Ejecutar el ejercicio 2
         elif opcion == "2":
             ejercicio2()
 
-        # Ejecutar el ejercicio 3
         elif opcion == "3":
             ejercicio3()
 
-        # Finalizar el programa
+        elif opcion == "4":
+            ejercicio4()
+
         elif opcion == "0":
             print("\nGracias por utilizar el programa.")
             break
 
-        # Validar una opción incorrecta
         else:
             print("\n❌ Opción no válida. Intente nuevamente.")
 
 
-# Verificar que este archivo se ejecute directamente
+# Verificar que el archivo se ejecute directamente
 if __name__ == "__main__":
     main()
